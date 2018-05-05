cafcd536186bdd76a56ab1b56139625bfa40d76f
Interest1024 <leo.liubin@gmail.com>
2018-05-05 22:47:11 +1000

    modify setting.py

--- censusplus_django/settings.py
+++ censusplus_django/settings.py
@@ -121,14 +121,14 @@ ALLOWED_HOSTS = ['censusplusdjango.herokuapp.com', '127.0.0.1', ]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATIC_DIRS = 'static'
-
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
